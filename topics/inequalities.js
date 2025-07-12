e9bd3dcb546c7c418dcd7ab71aa77406298af241
Anaymaheshwari <geetmah@gmail.com>
2025-07-12 14:38:26 +0530

    Update inequalities.js

--- topics/inequalities.js
+++ topics/inequalities.js
@@ -1,1 +1,36 @@
 //Logic code for Inequalities will go here
+// topics/inequalities.js
+
+// Solve inequality of form ax + b < c
+function solveLinearInequality(a, b, c, operator) {
+  const x = (c - b) / a;
+  return `x ${operator} ${x.toFixed(2)}`;
+}
+
+// Get inequality type from symbol
+function interpretInequality(symbol) {
+  switch (symbol) {
+    case '<': return "less than";
+    case '>': return "greater than";
+    case '<=': return "less than or equal to";
+    case '>=': return "greater than or equal to";
+    default: return "Invalid symbol";
+  }
+}
+
+// Generate number line explanation
+function numberLine(x, operator) {
+  switch (operator) {
+    case '<': return `All values to the left of ${x} (open circle)`;
+    case '>': return `All values to the right of ${x} (open circle)`;
+    case '<=': return `All values ≤ ${x} (closed circle)`;
+    case '>=': return `All values ≥ ${x} (closed circle)`;
+    default: return "Invalid operator";
+  }
+}
+
+export {
+  solveLinearInequality,
+  interpretInequality,
+  numberLine
+};
