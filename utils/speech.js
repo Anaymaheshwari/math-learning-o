03e81ea21342f0e25dabf1fc173c83e6eb2ab83f
Anaymaheshwari <geetmah@gmail.com>
2025-07-12 15:11:57 +0530

    Create speech.js

---
+++ utils/speech.js
@@ -0,0 +1,18 @@
+// utils/speech.js
+export function startVoiceInput(callback) {
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+  recognition.lang = 'en-IN';
+  recognition.interimResults = false;
+
+  recognition.onresult = (event) => {
+    const transcript = event.results[0][0].transcript;
+    callback(transcript);
+  };
+
+  recognition.onerror = (event) => {
+    alert("Error: " + event.error);
+  };
+
+  recognition.start();
+}
